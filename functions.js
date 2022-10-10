8238ad64ca5f21d93ea5bf5feeafd8c5bbfdea16
Amarilio de Oliveira <amarilioalencar@gmail.com>
2022-10-09 22:41:55 -0300

    modified:   functions.js

--- functions.js
+++ functions.js
@@ -76,5 +76,37 @@ function calculateBalance() {
 calculateBalance();
 
 // Conversor
-// de Celcius
+// de Celsius
 // para Fahrenheit
+function transformDegree(degree) {
+  const celsiusExists = degree.toUpperCase().includes("C");
+  const fahrenheitExists = degree.toUpperCase().includes("F");
+
+  if (!celsiusExists && !fahrenheitExists) {
+    throw new Error(
+      'Whoops, something got wrong! Only digit the temperature number plus "C" for (Celsius) or "F" for (Fahrenheit).'
+    );
+  }
+
+  // Fahrenheit to Celsius
+  let updatedDegree = Number(degree.toUpperCase().replace("F", ""));
+  let formula = (fahrenheit) => ((fahrenheit - 32) * 5) / 9;
+  let degreeSign = "°C";
+
+  // Celsius to Fahrenheit
+  if (celsiusExists) {
+    updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+    formula = (celsius) => (celsius * 9) / 5 + 32;
+    degreeSign = "°F";
+  }
+
+  return formula(updatedDegree) + degreeSign;
+}
+
+try {
+  console.log(transformDegree("10C"));
+  console.log(transformDegree("50F"));
+  transformDegree("50Z");
+} catch (error) {
+  console.log(error.message);
+}
