0b16e17f615fe9df266d7e8004e2f71b618d1d93
Amarilio de Oliveira <amarilioalencar@gmail.com>
2022-10-13 01:07:05 -0300

    modified:   functions.js

--- functions.js
+++ functions.js
@@ -152,3 +152,42 @@ const booksByCategory = [
 ];
 
 const totalCategories = booksByCategory.length;
+
+console.log(totalCategories);
+for (let category of booksByCategory) {
+  console.log("Book(s) total in the category:", category.category);
+  console.log(category.books.length);
+}
+
+function countAuthors() {
+  let authors = [];
+
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (authors.indexOf(book.author) == -1) {
+        authors.push(book.author);
+      }
+    }
+  }
+
+  console.log("Total authors:", authors.length);
+}
+
+countAuthors();
+
+function booksOfAuthor(author) {
+  let books = [];
+
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (book.author === author) {
+        books.push(book.title);
+      }
+    }
+  }
+
+  console.log(`Book(s) of ${author}: ${books.join(", ")}`);
+}
+
+booksOfAuthor("Augusto Cury");
+booksOfAuthor("George S. Clason");
